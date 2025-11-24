cf6d0fce2b2dc23e01c141d5333d797264d54998
BhavyankVith <34795451+BhavyankVith@users.noreply.github.com>
2025-11-24 19:31:13 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "demo-jenkins-test1"
     key            = "terraform.tfstate"
-    region         = "us-east-1"
+    region         = "us-east-2"
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
